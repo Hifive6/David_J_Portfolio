eef02a3e5164daec147bf48ad57354c5dc393ae2
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-03-23 15:55:52 -0400

    first commit all files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+//     $(window).scroll(function() { // check if scroll event happened
+//       if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+//         $(".fixed-top").css("background-color", "#f8f8f8"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+//       } else {
+//         $(".fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+//       }
+//     });
+//   });
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".scrollNav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
